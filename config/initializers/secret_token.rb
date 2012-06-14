2338a3ecc93059c6710660a9b7cbbd487b50c999
nbasu02 <nbasu02@gmail.com>
2012-06-14 16:01:29 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidDeviseSkeleton::Application.config.secret_token = '43226a180b56afb8d72ad040bf98440ffd924e618673390a00b378ef488ca98130c35db4f2a74f7f0af7caa7d09fb75801b88c5a932f15a46a10019e8c9f23fa'
